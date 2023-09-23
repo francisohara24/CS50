7364fe32b0863cff3910de21ae1f80b71b00e9e3
francisohara24 <franciskohara@gmail.com>
2023-09-22 21:08:58 -0400

    Feat: Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,5 @@
-// program for printing out a column of bricks of height 3
 
-#include <stdio.h>
-
-int main () {
-    char brick_symbol[] = "[x]";
-    int column_height = 3;
-
-    for (int i = 0; i < 3; i ++){
-        printf("%s \n", brick_symbol);
-    }
+int main(){
 
     return 0;
 }
